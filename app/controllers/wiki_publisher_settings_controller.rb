b8917ab1fada1c6f223deacc0c30cff042b14eaa
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2012-07-11 15:43:08 -0400

    Readme adicionado

--- app/controllers/wiki_publisher_settings_controller.rb
+++ app/controllers/wiki_publisher_settings_controller.rb
@@ -3,19 +3,13 @@ class WikiPublisherSettingsController < ApplicationController
 
   menu_item :wiki_publisher
   before_filter :find_project, :find_wiki
-  
   def edit
     design_repository_url = params[:wiki_publisher_setting][:design_repository_url]
     @wiki_publisher_setting = WikiPublisherSetting.find_or_create(@wiki.id)
-    if @wiki_publisher_setting
     @wiki_publisher_setting.design_repository_url = design_repository_url
     @wiki_publisher_setting.save!
-    #flash[:notice] = l(:notice_successful_update)
-    else
-    #flash[:error] = l(:error_failed_to_save_redmine_wiki_publisher_settings)
-    end
+
     redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => :wiki_publisher
-  #render(:update) {|page| page.replace_html "tab-content-wiki_publisher", :partial => 'projects/settings/wiki_publisher'}
   end
 
   private
--- init.rb
+++ init.rb
@@ -11,6 +11,6 @@ Redmine::Plugin.register :redmine_wiki_publisher do
   name 'Redmine Wiki Publisher plugin'
   author 'Heverson Vasconcelos'
   description 'This is a plugin for Redmine that publishes project documentation into wiki'
-  version '0.0.1'
+  version '0.0.9'
 
 end
