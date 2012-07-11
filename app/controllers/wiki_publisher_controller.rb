c4732d2f51f7a484512921dcad9f8a4479f6a4bb
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2012-07-11 10:39:12 -0400

    Correção dos locales

--- app/controllers/wiki_publisher_controller.rb
+++ app/controllers/wiki_publisher_controller.rb
@@ -3,63 +3,63 @@ require 'find'
 
 class WikiPublisherController < ApplicationController
   before_filter :find_wiki
-
   def import_wiki_files_from_repository
-     begin
-        files = find_repository_wiki_files
-        files.each do |file|
-           pagetitle = File.basename(file, '.' + @text_formatting)
-           @page = @wiki.find_or_new_page(pagetitle)
-           if(@page != nil)
-              @page.content = WikiContent.new(:page => @page)
-              @page.content.text = "{{repo_include(#{file})}}"
-              @page.save
-           end
+    begin
+      files = find_repository_wiki_files
+      files.each do |file|
+        pagetitle = File.basename(file, '.' + @text_formatting)
+        @page = @wiki.find_or_new_page(pagetitle)
+        if(@page != nil)
+          @page.content = WikiContent.new(:page => @page)
+          @page.content.text = "{{repo_include(#{file})}}"
+        @page.save
         end
-     rescue => e
-        flash[:warning] = e.message
-     end
+      end
+    rescue => e
+      flash[:warning] = e.message
+    end
 
-     redirect_to :controller => 'wiki', :action => 'show', :project_id => @project
+    redirect_to :controller => 'wiki', :action => 'show', :project_id => @project
   end
-   
-private
+
+  private
+
   def find_wiki
-     @project = Project.find(params[:project_id])
-     @wiki = @project.wiki
-     @text_formatting = Setting.text_formatting
+    @project = Project.find(params[:project_id])
+    @wiki = @project.wiki
+    @text_formatting = Setting.text_formatting
   end
 
   def find_repository
-     @repo = @project.repository
-     @wiki_publisher_setting = WikiPublisherSetting.find(:first, :conditions => ['wiki_id = ?', @wiki.id])
-     @design_repository_url = @wiki_publisher_setting.design_repository_url
-     
-     if(@repo.nil?)
-        raise "Falha ao acessar o repositório. Provavelmente não foi integrado ao Redmine corretamente"
-     elsif(@wiki_publisher_setting.nil? || @design_repository_url.nil?)
-        raise "Falha ao acessar o plugin Redmine Wiki Publisher. Provavelmente não foi instalado corretamente"
-     end
+    @repo = @project.repository
+    @wiki_publisher_setting = WikiPublisherSetting.find(:first, :conditions => ['wiki_id = ?', @wiki.id])
+    @design_repository_url = @wiki_publisher_setting.design_repository_url
+
+    if(@repo.nil?)
+      raise l(:error_failed_to_access_project_repository)
+    elsif(@wiki_publisher_setting.nil? || @design_repository_url.nil?)
+      raise l(:error_failed_to_access_redmine_wiki_publisher_settings)
+    end
   end
-  
+
   def find_repository_wiki_files
-     begin
-        #TODO: Refatorar este metodo para um helper que funcione para os diversos SCM
-        find_repository
-        Dir.chdir(Dir.tmpdir)
-        `svn co #{@repo.url}`
-        Dir.chdir(@project.identifier)
-        design_files = File.join(@design_repository_url, "**", "*." + @text_formatting)
-        repo_files = Dir.glob(design_files)
+    begin
+    #TODO: Refatorar este metodo para um helper que funcione para os demais SCM
+      find_repository
+      Dir.chdir(Dir.tmpdir)
+      `svn co #{@repo.url}`
+      Dir.chdir(@project.identifier)
+      design_files = File.join(@design_repository_url, "**", "*." + @text_formatting)
+      repo_files = Dir.glob(design_files)
 
-        if(repo_files.empty?)
-           raise "Falha ao encontrar os arquivos " + @text_formatting + ". Provavelmente a URL do plugin Redmine Wiki Publisher não foi informada corretamente"
-        end
-     rescue => e
-        raise e
-     end
+      if(repo_files.empty?)
+        raise l(:error_repository_wiki_files_not_found)
+      end
+    rescue => e
+      raise e
+    end
 
-     return repo_files
+    return repo_files
   end
 
 end
--- app/controllers/wiki_publisher_settings_controller.rb
+++ app/controllers/wiki_publisher_settings_controller.rb
@@ -1,32 +1,29 @@
-
-
 class WikiPublisherSettingsController < ApplicationController
-   unloadable
-   
-   menu_item :wiki_publisher
-   before_filter :find_project, :find_wiki
-   
-   verify :method => :post, :only => [:edit], :render => { :nothing => true, :status => :method_not_allowed }
+  unloadable
 
-   def edit
-     design_repository_url = params[:wiki_publisher_setting][:design_repository_url]
-     @wiki_publisher_setting = WikiPublisherSetting.find_or_create(@wiki.id)
-     if @wiki_publisher_setting
-        @wiki_publisher_setting.design_repository_url = design_repository_url
-        @wiki_publisher_setting.save!
-        #flash[:notice] = l(:notice_successful_update)
-     else
-        #flash[:error] = l(:notice_failed_to_save_redmine_wiki_publisher_settings)
-     end
-     redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => :wiki_publisher
-     #render(:update) {|page| page.replace_html "tab-content-wiki_publisher", :partial => 'projects/settings/wiki_publisher'}
-   end
-   
-private
-   def find_wiki
-     #@project = Project.find(params[:project_id])
-     @wiki = @project.wiki
-   end
+  menu_item :wiki_publisher
+  before_filter :find_project, :find_wiki
+  
+  def edit
+    design_repository_url = params[:wiki_publisher_setting][:design_repository_url]
+    @wiki_publisher_setting = WikiPublisherSetting.find_or_create(@wiki.id)
+    if @wiki_publisher_setting
+    @wiki_publisher_setting.design_repository_url = design_repository_url
+    @wiki_publisher_setting.save!
+    #flash[:notice] = l(:notice_successful_update)
+    else
+    #flash[:error] = l(:error_failed_to_save_redmine_wiki_publisher_settings)
+    end
+    redirect_to :controller => 'projects', :action => 'settings', :id => @project, :tab => :wiki_publisher
+  #render(:update) {|page| page.replace_html "tab-content-wiki_publisher", :partial => 'projects/settings/wiki_publisher'}
+  end
+
+  private
+
+  def find_wiki
+    #@project = Project.find(params[:project_id])
+    @wiki = @project.wiki
+  end
 
 end
 
